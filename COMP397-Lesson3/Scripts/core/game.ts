94b095a09d709f4607d25932f0e0f5bef276731e
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-10-01 03:36:59 -0400

    Added Stats and Started Contextual Commentation

--- COMP397-Lesson3/Scripts/core/game.ts
+++ COMP397-Lesson3/Scripts/core/game.ts
@@ -13,26 +13,40 @@
 // GLOBAL GAME FRAMEWORK VARIABLES
 var canvas: HTMLElement;
 var stage: createjs.Stage;
+var stats: Stats; //make a function to set up game stats - via this variable that is of class Stats (imported from Stats.d.js in Scripts/typings folder) 
 
 // Game Variables
 var helloLabel: objects.Label;
 var goodByeLabel: objects.Label;
 
+//Our void Start () method per se - upon intial frame, execute code
 function init():void {
     console.log("Game Started...");
     canvas = document.getElementById("canvas"); // reference to canvas element
     stage = new createjs.Stage(canvas); // passing canvas to stage
     createjs.Ticker.setFPS(60); // set frame rate to 60 fps
     createjs.Ticker.on("tick", gameLoop); // update gameLoop every frame
+    setupStats();//sets up our stat counting before calling Main function - to start counting in Main function
 
     main(); // call main game function
 }
 
-// Main Game Loop
+// Main Game Loop - Our void Update() per se - checks and executes code every frame/per frame
 function gameLoop(event: createjs.Event): void {
+    stats.begin(); //start counting per frame -
     stage.update(); // redraw/refresh stage every frame
+    stats.end(); // - stop counting per frame
 }
+//Setup Game Stats - FramesPerSecond, MilliSeconds Response Time in Latency, etc.
+function setupStats() {
+    stats = new Stats();
+    stats.setMode(0); // shows fps
+    stats.domElement.style.position = "absolute";//from the screen's exact position (absolute) -
+    stats.domElement.style.right = "0px";// - go 0px from the very right of the screen
+    stats.domElement.style.top = "0px";// - go 0px from very top of the screen- domElement translates to DivElement in JS
+    document.body.appendChild(stats.domElement); // translates to adding Stats to index.html in JS
 
+}
 // This is where all the fun happens
 function main(): void {
     helloLabel = new objects.Label("Hello World", "60px Consolas", "#000000", 320, 240);
